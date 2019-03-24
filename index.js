eb85f177ce3730bf6ab4c08fca3ed58fc7ea2cba
Capitan <oohla@mail.com>
2019-03-24 22:08:34 +0300

    Master - Add all charts

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const body = document.body;
-const options = { elem: body }
-const chart = new Chart(options);
-chart.build(data[4]);
+switch (document.location.pathname) {
+  case '/charts/first.html':
+    buildChart(0);
+    break;
+  case '/charts/second.html':
+    buildChart(1);
+    break;
+  case '/charts/third.html':
+    buildChart(2);
+    break;
+  case '/charts/fouth.html':
+    buildChart(3);
+    break;
+  case '/charts/fifth.html':
+    buildChart(4);
+    break;
+}
+
+function buildChart(num) {
+  const body = document.body;
+  const options = { elem: body }
+  const chart = new Chart(options);
+  chart.build(data[num]);
+}
